dc5ff06ea124b5957222594d3bdac773f1db93b4
Pablo Pera <pablo.sx@gmail.com>
2014-01-25 17:31:45 -0500

    Fixed potential NPE if the lottery ID is not consistent

--- client/Lottodroid/src/com/androidsx/lottodroid/view/QuinielaScoresView.java
+++ client/Lottodroid/src/com/androidsx/lottodroid/view/QuinielaScoresView.java
@@ -1,6 +1,9 @@
 package com.androidsx.lottodroid.view;
 
+import java.util.Date;
+
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -15,16 +18,18 @@ import com.androidsx.lottodroid.model.Quinigol;
 import com.androidsx.lottodroid.util.DateFormatter;
 
 public class QuinielaScoresView {
+  private static final String TAG = QuinielaScoresView.class.getSimpleName();
 
   public View createAndFillUpScoresView(Lottery lottery, Context context) {
-	
-	FootballLotery footballLotery = null;
-	
+	final FootballLotery footballLotery;
 	if(lottery.getId() == LotteryId.QUINIELA)
 		footballLotery = (Quiniela) lottery;
 	else if(lottery.getId() == LotteryId.QUINIGOL)
 		footballLotery = (Quinigol) lottery;
-	  
+	else {
+		footballLotery = new EmptyFootballLottery();
+		Log.e(TAG, "Didn't expect quiniela scores of type " + lottery.getId());
+	}
 	  
     View layoutView = View.inflate(context, R.layout.main_layout_row, null);
     LinearLayout layoutContent = (LinearLayout) layoutView.findViewById(R.id.layoutContent);
@@ -51,5 +56,54 @@ public class QuinielaScoresView {
     }
     
     return layoutView;
+  }
+  
+  /**
+   * Local <em>null object</em> for empty results for football lottery.
+   */
+  private static final class EmptyFootballLottery implements FootballLotery {
+	@Override
+	public LotteryId getId() {
+		throw new IllegalStateException();
+	}
+
+	@Override
+	public String getName() {
+		return "Sin resultados";
+	}
+
+	@Override
+	public Date getDate() {
+		return new Date();
+	}
+
+	@Override
+	public int getNumMatches() {
+		return 0;
+	}
+
+	@Override
+	public String getHomeTeam(int matchNumber) throws ArrayIndexOutOfBoundsException, NullPointerException {
+		return "";
+	}
+
+	@Override
+	public String getAwayTeam(int matchNumber) throws ArrayIndexOutOfBoundsException, NullPointerException {
+		return "";
+	}
+
+	@Override
+	public void setScore(int matchNumber, int homeScore, int awayScore) throws ArrayIndexOutOfBoundsException {
+	}
+
+	@Override
+	public int getHomeScore(int machtNumber) throws ArrayIndexOutOfBoundsException, NullPointerException {
+		return 0;
+	}
+
+	@Override
+	public int getAwayScore(int machtNumber) throws ArrayIndexOutOfBoundsException, NullPointerException {
+		return 0;
+	}
   }
 }
